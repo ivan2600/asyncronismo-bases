11900d417f25c9811c607ca9916114cff2bfc07b
Ivan Adamo <ivan.adamo.8@gmail.com>
2023-05-21 12:12:28 -0300

    challenge

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -1,9 +1,19 @@
-const XMLhttpRequest = require('xmlhttprequest');
+const XMLHttpRequest = require('xmlhttprequest');
 const API = 'https://api.escuelajs.co/api/v1';
 
 function fetchData(urlAPI, callback) {
-  let xhttp = newHttpRecuest();
+  let xhttp = new XMLHttpRequest();
 
-  xhttp.open('GET', urlApi, true);
-  xhttp.onready = statecance +
+  xhttp.open('GET', urlAPI, true);
+  xhttp.onreadystatechange = function (event) {
+    if(xhttp.readyState === 4) {
+      if (xhttp.status === 200) {
+        callback(null, JSON.parse(xhttp.responseText))
+      }
+    } else {
+      const error = new Error('Error' + urlAPI);
+      return callback(error, null);
+    }
+  }
+  xhttp.send();
 }
