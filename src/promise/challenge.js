adf35676595e76084004fe361862456c49f66326
Ivan Adamo <ivan.adamo.8@gmail.com>
2023-05-23 20:33:46 -0300

    fetch

--- src/promise/challenge.js
+++ src/promise/challenge.js
@@ -5,12 +5,30 @@ function fetchData(urlAPI) {
   return fetch(urlAPI);
 }
 
+// fetchData(`${API}/products`)
+//   .then(response => response.json())
+//   .then(products => {
+//     console.log(products)
+//   })
+//   .then(() => {
+//     console.log('Hola')
+//   })
+//   .catch(error => console.log('error'))
+
 fetchData(`${API}/products`)
   .then(response => response.json())
   .then(products => {
-    console.log(products)
+    console.log(products);
+    return fetchData(`${API}/products/${products[0].id}`)
   })
-  .then(() => {
-    console.log('Hola')
+  .then(response => response.json())
+  .then(product => {
+    console.log(product.title);
+    return fetchData(`${API}/categories/${product.category.id}`)
   })
-  .catch(error => console.log('error'))
+  .then(response => response.json())
+  .then(category => {
+    console.log(category.name);
+  })
+  .catch(err => console.log(err))
+  .finally(() => console.log('Finally'))
